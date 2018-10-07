e320c55a09e98f6940517a53a1703eedd5c65a3e
manaleks <manaleksdev@gmail.com>
2018-10-07 15:44:19 +0300

    try to run html template

--- application.py
+++ application.py
@@ -1,6 +1,21 @@
-from flask import Flask
+# -*- coding: utf8 -*-
+from flask import Flask, request, render_template
+try:
+  import simplejson as json
+except:
+  import json
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello Azure!"
+# Emotional Intelligence
+@app.route('/', methods=['GET'])
+def main_page():
+    return render_template('main.html')
+
+# Emotional Intelligence
+@app.route('/simple_text', methods=['GET'])
+def simple_text():
+    return "Hello!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
