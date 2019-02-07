641c673d7285f3844cfea1460dc545c6411e299b
manaleks <manaleksdev@gmail.com>
2019-02-07 21:27:27 +0300

    ready project?

---
+++ config.py
@@ -0,0 +1,20 @@
+test_db_config = {
+                "DBHOST":"localhost",
+                "DBUSER":"manager",
+                "DBNAME":"eventregistration",
+                "DBPASS":"supersecretpass"
+                }
+
+blockchain_db_config = {
+                        "DBHOST":"localhost",
+                        "DBUSER":"bockchaincontroller",
+                        "DBNAME":"bockchain",
+                        "DBPASS":"supersecretpass"
+                        }
+
+emotional_db_config = {
+                        "DBHOST":"localhost",
+                        "DBUSER":"emotional_manager",
+                        "DBNAME":"emotional_db",
+                        "DBPASS":"emotionalpass"
+                        }
